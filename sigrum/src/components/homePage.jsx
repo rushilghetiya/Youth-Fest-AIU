a6dc0325044147f13ef216d9adac0ea3e52be614
Anshuman <ab3649@srmist.edu.in>
2024-11-09 00:25:01 +0530

    primsa db

--- sigrum/src/components/homePage.jsx
+++
@@ -1,8 +0,0 @@
-// HomePage.jsx
-import React from "react";
-
-const HomePage = () => {
-  return <h1>Welcome to Home Page</h1>;
-};
-
-export default HomePage;
--- sigrum/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-// tailwind.config.js
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
